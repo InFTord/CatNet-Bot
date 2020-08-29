37aca439d1a3eba1d4c5d140c5e9a1f83b89c3ba
InFTord <60395110+InFTord@users.noreply.github.com>
2020-08-30 00:14:56 +0500

    Поменял название кога

--- cogs/fun-commands.py
+++ cogs/fun-commands.py
@@ -2,7 +2,7 @@ import discord # импортируем discord.py | pip install discord.py
 from discord.ext import commands # discord.ext - фреймворк для создания команд боту, идёт вместе с discord.py
 import asyncio # для работы с await / asyncio
 
-class cat(commands.Cog): # создаём класс с когом
+class fun-commands(commands.Cog): # создаём класс с когом
 	
 	def __init__(self, bot): # инициализация кога
 		self.bot = bot # создание переменной бота
@@ -15,4 +15,4 @@ class cat(commands.Cog): # создаём класс с когом
 
 def setup(bot): # функция
 	bot.add_cog(cat(bot)) # добавляем ког 
-	print(f"Ког cat готов!") # запуск кога
+	print(f"Ког fun-commands готов!") # запуск кога

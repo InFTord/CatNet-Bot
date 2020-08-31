5871d11de730410e1cd4b0e62e453493a8462be5
InFTord <60395110+InFTord@users.noreply.github.com>
2020-08-31 14:21:17 +0500

    Добавлена команда кика

--- catnetbot/cogs/moderation.py
+++ catnetbot/cogs/moderation.py
@@ -12,9 +12,7 @@ class Moderation(commands.Cog):
 		usage='[юзер] [причина]'
 	) 
 	@commands.has_permissions(kick_members=True)
-	async def kicking(self, ctx, member: discord.member, reason=None):
-		if not member:
-			return await ctx.send(embed = discord.Embed(description = f'**:warning: Правильное использование команды: `kick @пользователь причина`', color=0x800080))
+	async def kicking(self, ctx, member: discord.Member, *, reason=None):
 		if member.id == ctx.guild.owner.id:
 			return await ctx.send(embed = discord.Embed(description = f'**:warning: Данный пользователь, {member.mention}, является создателем этого сервера!**', color=0x800080))
 		if member.id == ctx.guild.me.id:
@@ -25,18 +23,26 @@ class Moderation(commands.Cog):
 			return await ctx.send(embed = discord.Embed(description = f'**:warning: Напомню, суицид - это не выход!**', color=0x800080))
 		if member.top_role > ctx.guild.me.top_role:
 			return await ctx.send(embed = discord.Embed(description = f'**:warning: Я не могу кикнуть {member.mention}, так как его роль выше моей!**', color=0x800080))
-			await ctx.guild.kick(member=member, reason=reason)
-
+			
 		if reason is None:
-			embed1 = discord.Embed(title=f"{ctx.author.name} кикнул {member.name}", description=f"Причина не указана.")
-			await ctx.send(embed=embed1)
 
-		else:
-			embed = discord.Embed(title=f"{ctx.author.name} кикнул {member.name}", description=f" Причина кика: { reason } ")
+			embed = discord.Embed(title="Кик", colour = discord.Color.red())
+			embed.add_field(name='Кто был кикнут?', value=f"{member.name}", inline=False)
+			embed.add_field(name='Кем был выдан кик?', value=f"{ctx.author.name}", inline=False)
+			embed.add_field(name='Причина кика?', value="Не указана", inline=False)
+			embed.set_footer(text=f"Фан факт, этот кик возможно попадет в КэтНет бота.",icon_url=ctx.message.author.avatar_url)
+			await member.kick(reason='Не указана.')
 			await ctx.send(embed=embed)
 
-
-
+		else:
+			embed1 = discord.Embed(title="Кик", colour = discord.Color.red())
+			embed1.add_field(name='Кто был кикнут?', value=f"{member.name}", inline=False)
+			embed1.add_field(name='Кем был выдан кик?', value=f"{ctx.author.name}", inline=False)
+			embed1.add_field(name='Причина кика?', value=f"{reason}", inline=False)
+			embed1.set_footer(text=f"Фан факт, этот кик возможно попадет в КэтНет бота.",icon_url=ctx.message.author.avatar_url)
+			await member.kick(reason=reason)
+			await ctx.send(embed=embed1)
+	
 
 
 def setup(bot):
